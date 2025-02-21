bd2b267298f120b3b1f61718aeaa431b64ec76b8
shariqsajad88 <bhatshariq88@gmail.com>
2025-02-21 10:26:17 +0530

    modified settings.py

--- auth_project/settings.py
+++ auth_project/settings.py
@@ -138,8 +138,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  # Or your preferred email provider
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'bhatshariq88@gmail.com'  # Replace with your email
-EMAIL_HOST_PASSWORD = 'rfnz wbyn yzlt ltit'
+EMAIL_HOST_USER = 'bhatshariq88@gmail.com' 
+EMAIL_HOST_PASSWORD = ''
 
 
 
